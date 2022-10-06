1e4029f69c6957f49fa09b834179cf9107c644b1
RubelMehmed <rubel.49@geography-juniv.edu.bd>
2022-10-06 18:59:46 +0600

    gitt

---
+++ array-1/travers.js
@@ -0,0 +1,28 @@
+const matrixA = [
+	[1, 3],
+	[1, 0],
+	[1, 2],
+];
+
+const  matrixB = [
+	[6, 0],
+	[7, 5],
+	[2, 1],
+];
+const matrixSum = (matrixA, matrixB) => {
+	const result = [];
+	for (let i = 0; i < matrixA.length; i++) {
+		const row = [];
+		for (let j = 0; j < matrixA[i].length; j++) {
+			row.push(matrixA[i][j] + matrixB[i][j]);
+		}
+		result.push(row);
+}
+return result;
+}
+const matrixC = matrixSum(matrixA, matrixB)
+console.log(matrixC);
+
+// to run 
+// "c:\projects\javascript\Array\travers-array\array-1\travers.js"
+// Ctrl+Alt+N
